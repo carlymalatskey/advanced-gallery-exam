7e36dc0a8586d914ff9360da8fc847f1c7a6e721
Carly Malatskey <cmalatskey@gmail.com>
2021-01-22 12:38:55 +0200

    add "back to top" link on navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -8,9 +8,9 @@ import "./NavBar.scss";
 class NavBar extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {}
-    }
-
+        this.state = {
+        }
+      }
     render() {
         return (
           <Nav className="nav-bar panel fixed-top navbar-expand-sm navbar-light bg-white"> 
@@ -18,11 +18,7 @@ class NavBar extends React.Component {
               <Image className="logo-image" src={LogoText} />
             </div>
             <div style={{display: "inline-flex"}}>
-              {this.props.tag.length > 0 ?
-                <a className="link-button search" href="App.js#app-root">Back To Top</a>            
-                :
-                <div></div> 
-              }
+              <a className="link-button search" href="/#app-root">Back To Top</a>            
               <h4 className="name-nav-bar">Welcome {this.props.name}!</h4>
               <a href="https://www.flickr.com/" target="_blank" className="link-button">Join the Flickr Community </a>
             </div>
