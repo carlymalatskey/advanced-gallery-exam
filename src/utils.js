2bbba85842253af695c75c1c6ef5000875843720
Carly Malatskey <cmalatskey@gmail.com>
2021-01-24 00:09:28 +0200

    combined move element helper functions into one

--- src/utils.js
+++ src/utils.js
@@ -1,14 +1,10 @@
-function move(array, sourceIndex, destinationIndex) {
+export function moveElement(array, sourceIndex, offset) {
+    const destinationIndex = sourceIndex + offset;
+  
     if (destinationIndex >= array.length) {
         destinationIndex = array.length - 1;
     }
     array.splice(destinationIndex, 0, array.splice(sourceIndex, 1)[0]);
 
     return array;
-  }
-  
-export function moveElement(array, sourceIndex, offset) {
-    const destinationIndex = sourceIndex + offset;
-  
-    return move(array, sourceIndex, destinationIndex);
 }
